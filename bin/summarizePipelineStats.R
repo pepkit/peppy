a5bf4dcff3cfa22272c8d8a096965a358f7e7797
sheffien <sheffien@users.noreply.github.com>
2015-05-05 08:29:09 +0200

    Add RRBS pipeline to summarizePipelineStats

--- bin/summarizePipelineStats.R
+++ bin/summarizePipelineStats.R
@@ -75,28 +75,40 @@ nofail = function(x) {
 	tryCatch( {x}, error = function(e) { message("Pipeline-specific summary error: ", e); } )
 }
 	#WGBS
+	if ("WGBS" %in% names(resultsMat) ) {
 	nofail( { resultsMat$WGBS[, total_efficiency := (Deduplicated_reads)/Raw_reads] })
 	nofail( { resultsMat$WGBS[, trim_loss_rate := (Raw_reads - Trimmed_reads)/Raw_reads] })
 	nofail( { resultsMat$WGBS[, alignment_rate := (Aligned_reads)/Trimmed_reads] })
 	nofail( { resultsMat$WGBS[, dupe_loss_rate := (Aligned_reads - Deduplicated_reads)/Aligned_reads] })
 	nofail( { resultsMat$WGBS[, filt_loss_rate := (Deduplicated_reads - Filtered_reads)/Deduplicated_reads] })
+	}
 
+	if ("RRBS" %in% names(resultsMat) ) {
+	nofail( { resultsMat$RRBS[, total_efficiency := (Aligned_reads)/Raw_reads] })
+	nofail( { resultsMat$RRBS[, trim_loss_rate := (Raw_reads - Trimmed_reads)/Raw_reads] })
+	nofail( { resultsMat$RRBS[, alignment_rate := (Aligned_reads)/Trimmed_reads] })
+	#nofail( { resultsMat$RRBS[, dupe_loss_rate := (Aligned_reads - Deduplicated_reads)/Aligned_reads] })
+	#nofail( { resultsMat$RRBS[, filt_loss_rate := (Deduplicated_reads - Filtered_reads)/Deduplicated_reads] })
+	}
 
 	# Tophat
+if ("rnaTopHat" %in% names(resultsMat) ) {
 	nofail( { resultsMat$rnaTopHat[, total_efficiency := Filtered_reads/Raw_reads] })
 	nofail( { resultsMat$rnaTopHat[, trim_loss_rate := (Raw_reads - Trimmed_reads)/Raw_reads] })
 	nofail( { resultsMat$rnaTopHat[, alignment_rate := (Aligned_reads)/Trimmed_reads] })
 	nofail( { resultsMat$rnaTopHat[, dupe_loss_rate := (Filtered_reads - Deduplicated_reads)/Filtered_reads] })
 	nofail( { resultsMat$rnaTopHat[, filt_loss_rate := (Aligned_reads - Filtered_reads)/Aligned_reads] })
+	}
 
 	# Bitseq
+if ("rnaBitSeq" %in% names(resultsMat) ) {
 	nofail( { resultsMat$rnaBitSeq[, total_efficiency := Filtered_reads/Raw_reads] })
 	nofail( { resultsMat$rnaBitSeq[, trim_loss_rate := (Raw_reads - Trimmed_reads)/Raw_reads] })
 	nofail( { resultsMat$rnaBitSeq[, alignment_rate := (Aligned_reads)/Trimmed_reads] })
 	nofail( { resultsMat$rnaBitSeq[, dupe_loss_rate := (Filtered_reads - Deduplicated_reads)/Filtered_reads] })
 	nofail( { resultsMat$rnaBitSeq[, filt_loss_rate := (Aligned_reads - Filtered_reads)/Aligned_reads] })
 	nofail( { resultsMat$rnaBitSeq[, ERCC_alignment_rate := (ERCC_aligned_reads)/Trimmed_reads] })
-
+	}
 
 ################################################################################
 # Write results
