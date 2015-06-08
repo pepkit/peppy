94311379189431947d08bb16e3c5bb05c03916fc
nsheff <nathan@code.databio.org>
2015-06-08 14:02:39 +0200

    update flagCheck to show up to 10 of each type

--- bin/flagCheck.sh
+++ bin/flagCheck.sh
@@ -5,5 +5,19 @@ echo "completed: $completed"
 echo "running: $running"
 echo "failed: $failed" 
 ls  */*.flag | xargs -n1 basename | sort | uniq -c
+
+if [ $failed -lt 10 ]; then
 echo "List of failed flags:"
 ls */*failed.flag 2> /dev/null
+fi
+
+if [ $completed -lt 10 ]; then
+echo "List of completed flags:"
+ls */*completed.flag 2> /dev/null
+fi
+
+if [ $running -lt 10 ]; then
+echo "List of running flags:"
+ls */*running.flag 2> /dev/null
+fi
+
