6ca5defe58df9f0717753d89425b7bcd11d60a20
sheffien <sheffien@users.noreply.github.com>
2015-10-19 16:12:31 +0200

    Add yaml resource table concept

--- submitter.py
+++ submitter.py
@@ -1,69 +1,137 @@
 import csv
 from collections import defaultdict
+import yaml
+import os
 
 class LurkerLogicTable(object):
-    def __init__(self, llt, prt):
-        self.pipelines = []
-        self.resources = []
-        self.dd = defaultdict(list)
+	def __init__(self, llt, prt):
+		self.pipelines = []
+		self.resources = defaultdict(dict)
+		self.dd = defaultdict(list)
 
-        f = open(llt, 'rb')  # opens the csv file
-        lurker_file = csv.reader(f, delimiter="\t")  # creates the reader object
+		f = open(llt, 'rb')  # opens the csv file
+		lurker_file = csv.reader(f, delimiter="\t")  # creates the reader object
 
-        for row in lurker_file: # iterates the rows of the file in orders
-            #print(row)
-            #self.pipelines.append( PipelineLogic(row) )
-            pipeline_name = row.pop(0)
-            pipeline_commands = filter(None, row)
-            #print(pipeline_name)
-            self.dd[pipeline_name].append(pipeline_commands)
+		for row in lurker_file: # iterates the rows of the file in orders
+			print(row)
+			#self.pipelines.append( PipelineLogic(row) )
+			pipeline_name = row.pop(0)
+			pipeline_commands = filter(None, row) #unlist
+			#print(pipeline_name)
+			self.dd[pipeline_name].append(pipeline_commands)
 
-    def build_pipeline(self, name):
-        print("Building pipeline " + name)
-        print(self.dd[name])
-        for value in self.dd[name]:
-            for i in range(0,len(value)):
-                if i == 0:
-                    self.parse_parallel_jobs(value[i], None)
-                else:
-                    self.parse_parallel_jobs(value[i], value[i-1])
+		f = open(prt, 'rb')  # opens the csv file
+		resource_file = csv.DictReader(f, delimiter="\t")  # creates the reader object
 
 
-    def parse_parallel_jobs(self, job, dep):
-        job = job.replace("(", "")
-        job = job.replace(")", "")
+		# Use the yaml config instead of this.
+		# outdated:
+#		for row in resource_file: # iterates the rows of the file in orders
+#			print(row)
+#			#self.pipelines.append( PipelineLogic(row) )
+#			pipeline_name = row.pop("PIPELINE")
+#			file_size = row.pop("MIN_GB")
+#			print(file_size)
+#			print(row)
+#			pipeline_commands = row#filter(None, row)
+#			#print(pipeline_name)
+#			self.resources[pipeline_name][file_size] = pipeline_commands
 
-        split_jobs = [x.strip() for x in job.split(',')]
-        if len(split_jobs) > 1:
-            for s in split_jobs:
-                self.submit_job(s, dep)
-        else:
-            self.submit_job(job, dep)
+	def getSize(filename):
+		st = os.stat(filename)
+		return st.st_size
 
-    def submit_job(self, job, dep, resources=None):
-        print("Submit. Name:" + job + "\tDep:" + str(dep) + "\tRes:" + str(resources))
+	def build_pipeline(self, name):
+		print("Building pipeline " + name)
+		print(self.dd[name])
+		for value in self.dd[name]:
+			for i in range(0,len(value)):
+				if i == 0:
+					self.parse_parallel_jobs(value[i], None)
+				else:
+					self.parse_parallel_jobs(value[i], value[i-1])
 
-    def printme(self):
-        for i in self.pipelines:
-            print(i)
 
-        print("dd:")
-        print(self.dd)
-        for i in self.dd:
-            print(i)
+	def parse_parallel_jobs(self, job, dep):
+		job = job.replace("(", "")
+		job = job.replace(")", "")
+
+		split_jobs = [x.strip() for x in job.split(',')]
+		if len(split_jobs) > 1:
+			for s in split_jobs:
+				self.submit_job(s, dep)
+		else:
+			self.submit_job(job, dep)
+
+	def submit_job(self, job, dep, resources=None):
+		print("Submit. Name:" + job + "\tDep:" + str(dep) + "\tRes:")
+		print("\t" +  str(self.resources[job]))
+		resource_profile = self.resource_lookup(82, self.resources[job])
+	
+		print("\t" +  str(self.resources[job][resource_profile]))
+
+
+
+
+	def printme(self):
+		for i in self.pipelines:
+			print(i)
+
+		print("dd:")
+		print(self.dd)
+		for i in self.dd:
+			print(i)
 
 
 class PipelineLogic(object):
-    def __init__(self, lst):
-        self.name = lst.pop(0)
-        self.commands = filter(None, lst)
-    def __str__(self):
-        return("Name: " + str(self.name) + "\tCommands: " + str(self.commands))
+	def __init__(self, lst):
+		self.name = lst.pop(0)
+		self.commands = filter(None, lst)
+	def __str__(self):
+		return("Name: " + str(self.name) + "\tCommands: " + str(self.commands))
+
+
+class ResourceTableLookup(object):
+	''' This class parses, holds, and returns information for a yaml 
+	resource file that specifies for given pipelines and file sizes, 
+	what resource variables should be used for the given file.
+	'''
+
+	def __init__(self, yaml_config_file):
+		#self.resources = defaultdict(dict)
+		self.resources = yaml.load(open(yaml_config_file, 'r'))
+		print(config)
+
+	def resource_lookup(self, tag, size):
+	'''
+	Given a pipeline name (tag) and a file size (size), return the
+	resource configuratio specified by the config file.
+	'''
+		current_pick=1000000000
+
+		table = self.resources[tag]
+		for alloc in table:
+			print(alloc)
+			if alloc == "default":
+				continue
+			if size > float(alloc):
+				continue
+			elif float(alloc) < current_pick:
+				current_pick = float(alloc)
+
+		if current_pick==1000000000:
+			current_pick = 'default'
+
+		print("choose:" + str(current_pick))
+		return(current_pick)
+
 
 
 psa = "basic_sample_table.tsv"
 llt = "lurker_logic_table.tsv"
 prt = "pipeline_resource_table.tsv"
+
+pry = "pipeline_resource_table.yaml"
 
 
 f = open(psa, 'rb')  # opens the csv file
@@ -71,28 +139,33 @@ processed_samples = {}
 
 input_file = csv.DictReader(f, delimiter="\t")  # creates the reader object
 
-f = open(prt, 'rb')  # opens the csv file
-resource_file = csv.DictReader(f, delimiter="\t")  # creates the reader object
 
+# We can use a yaml config file and then we don't have to parse as much.
+pipelines_config_file =  pry
+config = yaml.load(open(pipelines_config_file, 'r'))
+print(config)
+
+rtl = ResourceTableLookup(pipelines_config_file)
+rtl.resource_lookup("wgbs_pipeline.py", 29)
 
 # try:
 # 	input_file = csv.DictReader(f)  # creates the reader object
 # except:
-#     raise Exception("Can't read sample table.")
+#	 raise Exception("Can't read sample table.")
 #
 for row in input_file: # iterates the rows of the file in orders
-    print(row)
-    test=False
+	print(row)
+	test=False
 
 
-LLT = LurkerLogicTable(llt, prt)
-LLT.printme()
-print("build:")
-LLT.build_pipeline("CORE")
-LLT.build_pipeline("WGBSNM")
+#LLT = LurkerLogicTable(llt, prt)
+#LLT.printme()
+#print("build:")
+#LLT.build_pipeline("CORE")
+#LLT.build_pipeline("WGBSNM")
 
 
 #
 # for row in resource_file: # iterates the rows of the file in orders
-#     print(row)
-#     test=False
+#	 print(row)
+#	 test=False
