55ef8d0cd087b08b409ad20354c1664b2e1814eb
sheffien <sheffien@users.noreply.github.com>
2015-11-23 14:17:04 +0100

    revamp pipeline interface, other changes:
    - partial conversion of rrbs pipeline to new system

--- models.py
+++ models.py
@@ -52,6 +52,21 @@ class Paths(object):
 		return "Paths object."
 
 
+class AttributeDict:
+	"""
+	A class to convert a nested Dictionary into an object with key-values
+	accessibly using attribute notation (AttributeDict.attribute) instead of
+	key notation (Dict["key"]). This class recursively sets Dicts to objects,
+	allowing you to recurse down nested dicts (like: AttributeDict.attr.attr)
+	"""
+    def __init__(self, **entries):
+		for key, value in entries.items():
+			print(key, value)
+			if type(value) is dict:
+				self.__dict__[key] = AttributeDict(**value)
+			else:
+				self.__dict__.update(entries)
+
 class Project(object):
 	"""
 	A class to model a Project.
