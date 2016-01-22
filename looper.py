78b5864d63cfa09444f5206b7249abeb181c7fd9
Andre Rendeiro <afrendeiro@gmail.com>
2016-01-22 11:40:54 +0100

    add newline before "Not submitted"; fixed mixture of tabs and spaces

--- looper.py
+++ looper.py
@@ -111,7 +111,7 @@ def cluster_submit(
 		submit = False
 	else:
 		pass
-		#print("")  # Do you want to print a newline after every sample?
+		# print("")  # Do you want to print a newline after every sample?
 
 	if submit:
 		if dry_run:
@@ -127,36 +127,38 @@ def cluster_submit(
 
 
 def query_yes_no(question, default="no"):
-    """Ask a yes/no question via raw_input() and return their answer.
+	"""Ask a yes/no question via raw_input() and return their answer.
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
+	"question" is a string that is presented to the user.
+	"default" is the presumed answer if the user just hits <Enter>.
+		It must be "yes" (the default), "no" or None (meaning
+		an answer is required of the user).
 
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
+	The "answer" return value is True for "yes" or False for "no".
+	"""
+	valid = {
+		"yes": True, "y": True, "ye": True,
+		"no": False, "n": False}
+	if default is None:
+		prompt = " [y/n] "
+	elif default == "yes":
+		prompt = " [Y/n] "
+	elif default == "no":
+		prompt = " [y/N] "
+	else:
+		raise ValueError("invalid default answer: '%s'" % default)
 
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
+	while True:
+		sys.stdout.write(question + prompt)
+		choice = raw_input().lower()
+		if default is not None and choice == '':
+			return valid[default]
+		elif choice in valid:
+			return valid[choice]
+		else:
+			sys.stdout.write(
+				"Please respond with 'yes' or 'no' "
+				"(or 'y' or 'n').\n")
 
 
 def main():
@@ -187,7 +189,6 @@ def main():
 			print("Clean aborted by user.")
 			sys.exit(1)
 
-
 	# Create a few problem lists so we can keep track and show them at the end
 	failures = []
 
@@ -210,8 +211,6 @@ def main():
 				print(pipeline_outfolder + " does not exist.")
 
 			continue
-
-
 
 		# Don't submit samples with duplicate names
 		if sample.sample_name in prj.processed_samples:
@@ -239,7 +238,7 @@ def main():
 			fail = True
 
 		if fail:
-			print("Not submitted: " + fail_message)
+			print("\nNot submitted: " + fail_message)
 			failures.append([fail_message, sample.sample_name])
 			continue
 
