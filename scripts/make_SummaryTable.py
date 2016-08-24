f3bc58c47f5bbcebb97258fabbe5ba46bfcf2984
Nikolaus Fortelny <nfortelny@n001.hpc.cemm.at>
2016-08-24 15:28:02 +0200

    makeSummaryTable.py for multiple pipelines

--- scripts/make_SummaryTable.py
+++ scripts/make_SummaryTable.py
@@ -23,8 +23,28 @@ with open(args.config_file, 'r') as config_file:
 	config = AttributeDict(config_yaml, default=True)
 paths = config.paths
 
+
+
 if not os.path.exists(paths.output_dir):
 	raise Exception(paths.output_dir + " : project directory does not exist!")
+
+
+# FOR RIGID
+# #######################################################################################
+fields_in = []
+fields_out = []
+if args.rigid:
+	# the hard-coded fields for the legacy/rigid mode
+	fields_in = ['sample_name','instrument_model','flowcell','lane','read_length','Read_type','organism','Genome'\
+	,'cell_type','Raw_reads','Trimmed_reads','Trimmed_rate','Aligned_reads','Aligned_rate'\
+	,'Multimap_reads','Multimap_rate','Unique_CpGs','Total_CpGs','meanCoverage',\
+	'bisulfiteConversionRate','globalMethylationMean',\
+	'K1_unmethylated_count','K1_unmethylated_meth','K3_methylated_count','K3_methylated_meth']
+	fields_out = ['Sample','Instrument','Flowcell','Lane','Read Length','Read Type','Organism','Genome'\
+	,'Cell Type','Raw Reads','Trimmed Reads','Trimmed Rate','Aligned Reads','Aligned Rate'\
+	,'Multimap Reads','Multimap Rate','Unique CpGs','Total CpGs','Mean Coverage',\
+	'Bisulfite Conversion Rate',' Global Methylation Mean',\
+	'K1 Unmethylated Count','K1 Unmethylated Meth','K3 Methylated Count','K3 Methylated Meth']
 
 
 # Open samples CSV file
@@ -39,48 +59,12 @@ else:
 csv_reader = csv.DictReader(csv_file)
 
 
-# Output TSV file
-# #######################################################################################
-tsv_outfile_path = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+'_stats_summary.tsv')
-tsv_outfile = open(tsv_outfile_path, 'w')
-
-fields_in = []
-fields_out = []
-if args.rigid:
-	# the hard-coded fields for the legacy/rigid mode
-	fields_in = ['sample_name','instrument_model','flowcell','lane','read_length','read_type','organism','Genome'\
-	,'cell_type','Raw_reads','Trimmed_reads','Trimmed_rate','Aligned_reads','Aligned_rate'\
-	,'Multimap_reads','Multimap_rate','Unique_CpGs','Total_CpGs','meanCoverage',\
-	'bisulfiteConversionRate','globalMethylationMean',\
-	'K1_unmethylated_count','K1_unmethylated_meth','K3_methylated_count','K3_methylated_meth']
-	fields_out = ['Sample','Instrument','Flowcell','Lane','Read Length','Read Type','Organism','Genome'\
-	,'Cell Type','Raw Reads','Trimmed Reads','Trimmed Rate (%)','Aligned Reads','Aligned Rate (%)'\
-	,'Multimap Reads','Multimap Rate (%)','Unique CpGs','Total CpGs','Mean Coverage',\
-	'Bisulfite Conversion Rate (%)',' Global Methylation Mean',\
-	'K1 Unmethylated Count','K1 Unmethylated Meth','K3 Methylated Count','K3 Methylated Meth']
-	tsv_writer = csv.DictWriter(tsv_outfile, fieldnames=fields_out, delimiter='\t')
-	tsv_writer.writeheader()
-
-global_list = []
-global_keys = []
-
-
-# Output XLS file
-# #######################################################################################
-if args.excel:
-
-	import xlwt
-
-	xls_book = xlwt.Workbook(encoding="utf-8")
-	xls_sheet_name = "Stats"
-	xls_sheet = xls_book.add_sheet(xls_sheet_name)
-	if args.rigid:
-		for i,field in enumerate(fields_out):
-			xls_sheet.write(0, i, field)
-
-
 # Looping over all samples
 # #######################################################################################
+global_list = dict()
+global_keys = dict()
+
+pipelines = []
 sample_count = 0
 column_count = 0
 print("\nStart iterating over samples")
@@ -106,20 +90,175 @@ for row in csv_reader:
 			print("Found: " + stat_file_path)
 		else:
 			raise Exception(stat_file_path + " : file does not exist!")
+
+
 		stats_dict = dict()
-		stats_dict_keys = []
+		stats_dict_keys = dict()
+
+		# Check if file has third column -> define pipelines based on that
+		# plus read info from file
+
 		for line in stat_file:
 			line_content = line.split('\t')
-                        key = line_content[0]
-                        value = line_content[1]
-			stats_dict[key] = value.strip()
-			stats_dict_keys.append(key)
+			key = line_content[0]
+			value = line_content[1]
+			pip = "x"
+			if len(line_content) == 3: 
+				pip = line_content[2].strip()
+			pipelines.append(pip)
+			if not pip in stats_dict: stats_dict[pip] = dict()
+			if not pip in stats_dict_keys: stats_dict_keys[pip] = []
+			stats_dict[pip][key] = value.strip()
+			stats_dict_keys[pip].append(key)
 
+		pipelines = list(set(pipelines))
+		print "Pipelines: " + str(pipelines)
+
+
+		# stats_dict and stats_dict_keys are pipeline specific
+		for pip in pipelines:
+			if not pip in global_list: global_list[pip] = []
+			if not pip in global_keys: global_keys[pip] = []
+
+
+
+		# if there are two pipelines make sure that certain values are present in both
+		missing_cols = ["Raw_reads", "Fastq_reads", "Trimmed_reads", "Trim_loss_rate"]
+		if len(pipelines) == 2:
+			for col in missing_cols:
+				if not col in stats_dict[pipelines[1]] and col in stats_dict[pipelines[0]]: stats_dict[pipelines[1]][col] = stats_dict[pipelines[0]][col]
+				if not col in stats_dict[pipelines[0]] and col in stats_dict[pipelines[1]]: stats_dict[pipelines[0]][col] = stats_dict[pipelines[1]][col]
+				for pip in pipelines:
+					stats_dict_keys[pip] = list(set(stats_dict_keys[pip] + missing_cols))
+
+		# Write to global list and keys
 		new_row = dict()
 		column_count = 0
+		for pip in pipelines:
+			new_row = row.copy()
+			new_row.update(stats_dict[pip])
+			global_list[pip].append(new_row)
+			global_keys[pip] = csv_reader.fieldnames + stats_dict_keys[pip]
 
-		if args.rigid:
 
+	except Exception as e:
+
+		print("Sample " + sample_name + " failed. Error: " + str(e))
+
+csv_file.close()
+# print global_keys
+# print global_list
+
+# Writing to Output Files
+# #######################################################################################
+if not args.rigid:
+	
+	# Writing TSV file
+	# #######################################################################################
+
+	for pip in pipelines:
+		
+		pip_nam = "_" + pip
+		if pip_nam == "_x": pip_nam = ""
+		tsv_outfile_path = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+ pip_nam + '_stats_summary.tsv')
+		tsv_outfile = open(tsv_outfile_path, 'w')
+	
+		if global_list[pip] and global_keys[pip]:
+
+			tsv_writer = csv.DictWriter(tsv_outfile, fieldnames=global_keys[pip], delimiter='\t')
+			tsv_writer.writeheader()
+
+			for i,sample in enumerate(global_list[pip]):
+				tsv_writer.writerow(sample)
+				if args.excel:
+					for j,field in enumerate(global_keys[pip]):
+						if i == 0: xls_sheet.write(0, j, field)
+						xls_sheet.write(i+1, j, sample[field])
+
+		tsv_outfile.close()
+
+	print("\nInput used: " + csv_file_path)
+	print("Results TSV file: " + tsv_outfile_path)
+
+
+
+
+	# Output XLS file
+	# #######################################################################################
+	if args.excel:
+		
+		raise Exception("--excel not implemented")
+			
+		import xlwt
+		
+		for pip in pipelines:
+			
+			pip_nam = "_" + pip
+			if pip_nam == "_x": pip_nam = ""
+
+			xls_book = xlwt.Workbook(encoding="utf-8")
+			xls_sheet_name = "Stats" + pip_nam
+			xls_sheet = xls_book.add_sheet(xls_sheet_name)
+
+			# Where should this be written? Here or below?
+			# if args.rigid:
+			# 	for i,field in enumerate(fields_out):
+			# 		xls_sheet.write(0, i, field)
+
+			import xlrd
+			import openpyxl
+
+			# saving the XLS sheet
+			xls_filename = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+'_stats_summary.xls')
+			xls_book.save(xls_filename)
+			print("Results XLS file: " + xls_filename)
+
+			# convert XLS sheet to XLSX format
+			xlsx_book_in = xlrd.open_workbook(xls_filename)
+			index = 0
+			nrows = sample_count + 2
+			ncols = 0
+			if global_keys[pip]: ncols = len(global_keys[pip])
+			else: ncols = column_count
+			ncols += 1
+			xlsx_sheet_in = xlsx_book_in.sheet_by_index(0)
+			xlsx_book_out = openpyxl.Workbook()
+			xlsx_sheet = xlsx_book_out.active
+			xlsx_sheet.title = xls_sheet_name
+			for row in range(1, nrows):
+				for col in range(1, ncols):
+					xlsx_sheet.cell(row=row, column=col).value = xlsx_sheet_in.cell_value(row-1, col-1)
+			xlsx_filename = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+'_stats_summary.xlsx')
+			xlsx_book_out.save(xlsx_filename)
+			print("Results XLSX file: " + xlsx_filename)
+
+	print("\n")
+
+
+
+# RIGID		
+else:
+	if args.excel:
+		raise Exception("--excel not implemented for option --rigid")
+		
+	for pip in pipelines:
+
+		pip_nam = "_" + pip
+		if pip_nam == "_x": pip_nam = ""
+		# Open file to write to
+		tsv_outfile_path = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+ pip_nam + '_stats_summary.tsv')
+		tsv_outfile = open(tsv_outfile_path, 'w')
+		tsv_writer = csv.DictWriter(tsv_outfile, fieldnames=fields_out, delimiter='\t')
+		tsv_writer.writeheader()
+		
+		
+
+		
+		# for each sample data (one element of the global list)
+		for sample_dict in global_list[pip]:
+			
+			new_row = dict()
+			# Write each field
 			for i in range(0,len(fields_in)):
 
 				field = fields_in[i]
@@ -129,19 +268,22 @@ for row in csv_reader:
 				content_int = int(-1)
 
 				# extract all the listed fields
-				if field == 'Trimmed_rate':
-					content_float = 100.0*float(stats_dict['Trimmed_reads'])/float(stats_dict['Raw_reads'])
-				elif field == 'Aligned_rate':
-					content_float = 100.0*float(stats_dict['Aligned_reads'])/float(stats_dict['Trimmed_reads'])
-				elif field == 'Multimap_rate':
-					content_float = 100.0*float(stats_dict['Multimap_reads'])/float(stats_dict['Trimmed_reads'])
-				elif row.has_key(field):
-					content = str(row[field].strip())
-				elif stats_dict.has_key(field):
-					content = str(stats_dict[field])
-				else:
+				# some data types might not have all the fields in stats_dict, then catch the KeyError
+				try:
+					if field == 'Trimmed_rate':
+						content_float = 100.0*float(sample_dict['Trimmed_reads'])/float(sample_dict['Raw_reads'])
+					elif field == 'Aligned_rate':
+						content_float = 100.0*float(sample_dict['Aligned_reads'])/float(sample_dict['Trimmed_reads'])
+					elif field == 'Multimap_rate':
+						content_float = 100.0*float(sample_dict['Multimap_reads'])/float(sample_dict['Trimmed_reads'])
+					elif sample_dict.has_key(field):
+						content = str(sample_dict[field].strip())
+					else:
+						content = 'NA'
+						print("No field called: " + field)
+				except KeyError:
 					content = 'NA'
-					print("No field called: " + field)
+					print("Data missing to calculate: " + field)
 
 				# convert str to float or int if needed
 				got_comma = content.find('.')
@@ -165,77 +307,10 @@ for row in csv_reader:
 					column_count += 1
 					new_row[field_out] = content
 					if args.excel: xls_sheet.write(sample_count, i, content)
-
+			
 			tsv_writer.writerow(new_row)
-
-		else:
-			new_row = row.copy()
-			new_row.update(stats_dict)
-			global_list.append(new_row)
-			global_keys = csv_reader.fieldnames + stats_dict_keys
-
-	except Exception as e:
-
-		print("Sample " + sample_name + " failed. Error: " + str(e))
-
-csv_file.close()
-
-# Writing TSV file from the global list
-# #######################################################################################
-if global_list and global_keys:
-
-	tsv_writer = csv.DictWriter(tsv_outfile, fieldnames=global_keys, delimiter='\t')
-	tsv_writer.writeheader()
-
-	for i,sample in enumerate(global_list):
-		tsv_writer.writerow(sample)
-		if args.excel:
-			for j,field in enumerate(global_keys):
-				if i == 0: xls_sheet.write(0, j, field)
-				xls_sheet.write(i+1, j, sample[field])
-
-tsv_outfile.close()
-
-print("\nInput used: " + csv_file_path)
-print("Results TSV file: " + tsv_outfile_path)
-
-# The ugly
-# #######################################################################################
-if args.excel:
-
-	import xlrd
-	import openpyxl
-
-	# saving the XLS sheet
-	xls_filename = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+'_stats_summary.xls')
-	xls_book.save(xls_filename)
-	print("Results XLS file: " + xls_filename)
-
-	# convert XLS sheet to XLSX format
-	xlsx_book_in = xlrd.open_workbook(xls_filename)
-	index = 0
-	nrows = sample_count + 2
-	ncols = 0
-	if global_keys: ncols = len(global_keys)
-	else: ncols = column_count
-	ncols += 1
-	xlsx_sheet_in = xlsx_book_in.sheet_by_index(0)
-	xlsx_book_out = openpyxl.Workbook()
-	xlsx_sheet = xlsx_book_out.active
-	xlsx_sheet.title = xls_sheet_name
-	for row in range(1, nrows):
-		for col in range(1, ncols):
-			xlsx_sheet.cell(row=row, column=col).value = xlsx_sheet_in.cell_value(row-1, col-1)
-	xlsx_filename = os.path.join(paths.output_dir,os.path.basename(paths.output_dir)+'_stats_summary.xlsx')
-	xlsx_book_out.save(xlsx_filename)
-	print("Results XLSX file: " + xlsx_filename)
-
-print("\n")
-
-
-
-
-
+		
+	tsv_outfile.close()
 
 
 
