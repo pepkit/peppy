dfb23824deca6fef41d3d78101a5487048ef6ceb
Vince Reuter <vince.reuter@gmail.com>
2017-02-07 14:26:09 -0500

    remove bad q flag from pytest addopts; ignore pytest-related metastuff; more pytest-related modifications to other test set

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,6 +13,7 @@ class ExampleObject():
 
 
 def test_copy():
+    """ Test reference and equivalence comparison operators. """
     obj = ExampleObject()
     new_obj = copy(obj)
     assert obj is new_obj
@@ -21,6 +22,7 @@ def test_copy():
 
 @pytest.mark.parametrize(argnames="attr", argvalues=_ATTR_VALUES)
 def test_Paths(attr):
+    """ Check that Paths attribute can be set and returned as expected. """
     paths = Paths()
     paths.example_attr = attr
     assert getattr(paths, "example_attr") is attr
@@ -29,7 +31,7 @@ def test_Paths(attr):
 @pytest.mark.parametrize(argnames="attval",
                          argvalues=_ATTR_VALUES + [np.random.random(20)])
 def test_AttributeDict_setret(attval):
-    """ Test attribute setting and retrieval. """
+    """ Test attr fetch with both dictionary syntax and with object syntax. """
     # Set and retrieve attributes
     attrd = AttributeDict({"attr": attval})
     assert attrd["attr"] is attval
@@ -44,4 +46,5 @@ def test_AttributeDict_nested(attval):
     attrd.attrd = AttributeDict({"attr": attval})
     assert attrd.attrd["attr"] is attval
     assert getattr(attrd.attrd, "attr") is attval
+    assert attrd["attrd"].attr is attval
 
