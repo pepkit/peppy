f425019d72f1186f88dd0e3b0990b5ec29b6a71d
Vince Reuter <vince.reuter@gmail.com>
2017-03-01 13:37:50 -0500

    log at positive val

--- looper/models.py
+++ looper/models.py
@@ -136,7 +136,7 @@ class AttributeDict(MutableMapping):
         :param collections.Iterable | collections.Mapping entries: collection
             of pairs of keys and values
         """
-        self._log_(0, "Adding entries {}".format(entries))
+        self._log_(5, "Adding entries {}".format(entries))
         # Permit mapping-likes and iterables/generators of pairs.
         if callable(entries):
             entries = entries()
