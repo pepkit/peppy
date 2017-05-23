32af19f28879ec1c00440e4bc37ad341df4c34f7
Vince Reuter <vince.reuter@gmail.com>
2017-05-23 19:24:56 -0400

    finish the negative test, that lacking sample annotations causes Project ctor exception

--- tests/models/independent/test_Project.py
+++ tests/models/independent/test_Project.py
@@ -2,6 +2,7 @@
 
 import os
 import pytest
+import yaml
 from looper.models import Project, MissingMetadataException
 
 
@@ -13,12 +14,22 @@ __email__ = "vreuter@virginia.edu"
 class ProjectRequirementsTests:
     """ Tests for a Project's set of requirements. """
 
+    NO_ANNOTATIONS_CONFIG_DATA = \
+        {"metadata": {
+            "output_dir": "$HOME/sequencing/output",
+            "pipelines_dir": "${CODE}/pipelines"},
+        "data_sources": {"arbitrary": "placeholder/data/{filename}"},
+        "genomes": {"human": "hg19", "mouse": "mm10"},
+        "transcriptomes": {"human": "hg19_cdna", "mouse": "mm10_cdna"}}
 
-    def test_lacks_sample_annotations(self):
+
+    def test_lacks_sample_annotations(self, env_config_filepath, tmpdir):
         """ Lack of sample annotations precludes Project construction. """
+        conf_path = tmpdir.join("proj-conf.yaml").strpath
+        with open(conf_path, 'w') as conf_file:
+            yaml.safe_dump(self.NO_ANNOTATIONS_CONFIG_DATA, conf_file)
         with pytest.raises(MissingMetadataException):
-            # TODO: implement
-            raise Exception()
+            Project(conf_path, default_compute=env_config_filepath)
 
 
     def test_minimal_configuration_doesnt_fail(
