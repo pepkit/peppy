2517d4747fb3df34a3ee0dcabb5c89d09c5430d3
Vince Reuter <vince.reuter@gmail.com>
2017-11-15 18:05:50 -0500

    usage docs update find-replace

--- update-usage-docs.sh
+++ update-usage-docs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cp doc/source/usage.template usage.template
-#looper --help > USAGE.temp 2>&1
+#pep --help > USAGE.temp 2>&1
 
 for cmd in "--help" "run --help" "summarize --help" "destroy --help" "check --help" "clean --help" "--help"; do
 	echo $cmd
-	echo -e "\n\`\`looper $cmd\`\`" > USAGE_header.temp
+	echo -e "\n\`\`pep $cmd\`\`" > USAGE_header.temp
 	echo -e "----------------------------------" >> USAGE_header.temp
-	looper $cmd --help > USAGE.temp 2>&1
+	pep $cmd --help > USAGE.temp 2>&1
 	sed -i 's/^/\t/' USAGE.temp
 	sed -i '1s/^/\n.. code-block:: none\n\n/' USAGE.temp
-	#sed -i -e "/\`looper $cmd\`/r USAGE.temp" -e '$G' usage.template  # for -in place inserts
+	#sed -i -e "/\`pep $cmd\`/r USAGE.temp" -e '$G' usage.template  # for -in place inserts
 	cat USAGE_header.temp USAGE.temp >> usage.template # to append to the end
 done
 rm USAGE.temp
