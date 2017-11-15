441112ded622997e0daa811e6de2634d0a38c791
Vince Reuter <vince.reuter@gmail.com>
2017-11-14 22:15:29 -0500

    less alarming red

--- looper/looper.py
+++ looper/looper.py
@@ -537,10 +537,9 @@ class Runner(Executor):
         if failed_submission_scripts:
             _LOGGER.info(
                     Fore.LIGHTRED_EX +
-                    "\n{} scripts with failed submission: {}".
-                    format(len(failed_submission_scripts),
-                           ", ".join(failed_submission_scripts)) +
-                    Style.RESET_ALL)
+                    "\n{} scripts with failed submission: ".
+                    format(len(failed_submission_scripts)) + Style.RESET_ALL +
+                    ", ".join(failed_submission_scripts))
 
 
 
