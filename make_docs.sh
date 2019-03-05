38884296a35924e766be2d3226e958b2199c776e
nsheff <nsheff@users.noreply.github.com>
2019-03-05 10:56:58 -0500

    remove make_docs.sh

--- make_docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-peppy_home="${CODE}/peppy"
-docs_home="${peppy_home}/docs/docs"
-pydocmd simple peppy+ peppy.project++ peppy.sample++ peppy.utils++ > ${docs_home}/autodoc_build/peppy.md
