428173d9fd2f06e673ced47bb53c841a849fa124
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-04-24 13:57:27 -0400

    more test coverage

--- tests/smoketests/test_Project.py
+++ tests/smoketests/test_Project.py
@@ -1,6 +1,8 @@
 """ Classes for peppy.Project smoketesting """
 
 from peppy import Project
+from peppy.exceptions import MissingAmendmentError
+from pandas import DataFrame
 import pytest
 
 __author__ = "Michal Stolarczyk"
@@ -9,7 +11,7 @@ __email__ = "michal@virginia.edu"
 EXAMPLE_TYPES = \
     ["basic", "derive", "imply", "append", "amendments1", "amendments2",
      "derive_imply", "duplicate", "imports", "subtable1", "subtable2",
-     "subtable3", "subtable4", "subtable5"]
+     "subtable3", "subtable4", "subtable5", "remove"]
 
 
 class ProjectConstructorTests:
@@ -22,10 +24,96 @@ class ProjectConstructorTests:
     @pytest.mark.parametrize('example_pep_cfg_path', EXAMPLE_TYPES, indirect=True)
     def test_instantiaion(self, example_pep_cfg_path):
         """
-        Verify that Project object is succesfully created for every example PEP
+        Verify that Project object is successfully created for every example PEP
         """
         p = Project(cfg=example_pep_cfg_path)
         assert isinstance(p, Project)
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
+    def test_amendments(self, example_pep_cfg_path):
+        """
+        Verify that the amendment is activate at object instantiation
+        """
+        p = Project(cfg=example_pep_cfg_path, amendments="newLib")
+        assert all([s["protocol"] == "ABCD" for s in p.samples])
+
+
+class ProjectManipulationTests:
+    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
+    def test_amendments_activation_interactive(self, example_pep_cfg_path):
+        """
+        Verify that the amendment can be activated interactively
+        """
+        p = Project(cfg=example_pep_cfg_path)
+        p.activate_amendments("newLib")
+        assert all([s["protocol"] == "ABCD" for s in p.samples])
+        assert p.amendments is not None
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
+    def test_amendments_deactivation_interactive(self, example_pep_cfg_path):
+        """
+        Verify that the amendment can be activated interactively
+        """
+        p = Project(cfg=example_pep_cfg_path)
+        p.deactivate_amendments()
+        assert all([s["protocol"] != "ABCD" for s in p.samples])
+        p.activate_amendments("newLib")
+        p.deactivate_amendments()
+        assert all([s["protocol"] != "ABCD" for s in p.samples])
+        assert p.amendments is None
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
+    def test_missing_amendment_raises_error(self, example_pep_cfg_path):
+        """
+        Verify that the missing amendment request raises correct exception
+        """
+        with pytest.raises(MissingAmendmentError):
+            Project(cfg=example_pep_cfg_path, amendments="nieznany")
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
+    def test_missing_amendment_raises_error(self, example_pep_cfg_path):
+        """
+        Verify that the missing amendment request raises correct exception
+        """
+        with pytest.raises(MissingAmendmentError):
+            Project(cfg=example_pep_cfg_path, amendments="nieznany")
+
+    @pytest.mark.parametrize('example_pep_cfg_path', EXAMPLE_TYPES, indirect=True)
+    def test_str_repr_correctness(self, example_pep_cfg_path):
+        """
+        Verify that the missing amendment request raises correct exception
+        """
+        p = Project(cfg=example_pep_cfg_path)
+        str_repr = p.__str__()
+        assert example_pep_cfg_path in str_repr
+        assert "{} samples".format(str(len(p.samples))) in str_repr
+        assert p.name in str_repr
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
+    def test_amendments_listing(self, example_pep_cfg_path):
+        """
+        Verify that the missing amendment request raises correct exception
+        """
+        p = Project(cfg=example_pep_cfg_path)
+        assert isinstance(p.list_amendments, list)
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["basic"], indirect=True)
+    def test_sample_updates_regenerate_df(self, example_pep_cfg_path):
+        """
+        Verify that Sample modifications cause sample_table regeneration
+        """
+        p = Project(cfg=example_pep_cfg_path)
+        s_ori = p.sample_table
+        p.samples[0].update({"witam": "i_o_zdrowie_pytam"})
+        assert not p.sample_table.equals(s_ori)
+
+    @pytest.mark.parametrize('example_pep_cfg_path', ["subtable1"], indirect=True)
+    def test_subsample_table_property(self, example_pep_cfg_path):
+        """
+        Verify that Sample modifications cause sample_table regeneration
+        """
+        p = Project(cfg=example_pep_cfg_path)
+        assert isinstance(p.subsample_table, DataFrame)
 
 
 class SampleModifiersTests:
@@ -77,14 +165,6 @@ class SampleModifiersTests:
         p = Project(cfg=example_pep_cfg_path)
         assert all(["protocol" not in s for s in p.samples])
 
-    @pytest.mark.parametrize('example_pep_cfg_path', ["amendments1"], indirect=True)
-    def test_amendments(self, example_pep_cfg_path):
-        """
-        Verify that the amendment is activate at object instantiation
-        """
-        p = Project(cfg=example_pep_cfg_path, amendments="newLib")
-        assert all([s["protocol"] == "ABCD" for s in p.samples])
-
     @pytest.mark.parametrize('example_pep_cfg_path', ["subtable2"], indirect=True)
     def test_subtable(self, example_pep_cfg_path):
         """
@@ -93,13 +173,3 @@ class SampleModifiersTests:
         p = Project(cfg=example_pep_cfg_path)
         assert all([isinstance(s["file"], list) for s in p.samples
                     if s["sample_name"] in ["frog_1", "frog2"]])
-
-    @pytest.mark.parametrize('example_pep_cfg_path', ["basic"], indirect=True)
-    def test_sample_updates_regenerate_df(self, example_pep_cfg_path):
-        """
-        Verify that Sample modifications cause sample_table regeneration
-        """
-        p = Project(cfg=example_pep_cfg_path)
-        s_ori = p.sample_table
-        p.samples[0].update({"witam": "i_o_zdrowie_pytam"})
-        assert not p.sample_table.equals(s_ori)
