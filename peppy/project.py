d981957fa9176ea32e7ab89740b47cf05c7cf8a0
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-05-04 09:13:48 -0400

    use const

--- peppy/project.py
+++ peppy/project.py
@@ -538,7 +538,7 @@ class Project(PathExAttMap):
         if CONFIG_FILE_KEY in self:
             msg += " ({})".format(self[CONFIG_FILE_KEY])
         if DESC_KEY in self and self[DESC_KEY] is not None:
-            msg += "\ndescription: {}".format(self[DESC_KEY])
+            msg += "\n{}: {}".format(DESC_KEY, self[DESC_KEY])
         try:
             num_samples = len(self._samples)
         except (AttributeError, TypeError):
