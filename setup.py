5cf6aca8e4ac9fc22866dac64c7f21f18cf860f9
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2021-06-15 10:54:53 -0400

    bring old pandas support for py3.6

--- setup.py
+++ setup.py
@@ -83,7 +83,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
